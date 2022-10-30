8a9fc4fe3322b5cda7f8d6efa373f22b5082fad6
Ulugbek <ubakhromov22@gmail.com>
2022-10-30 09:59:18 +0500

    Proof Of Concept

---
+++ xChanger.Core.POC/Services/Coordinations/IExternalPersonWithPetsCoordinationService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using xChanger.Core.POC.Models.Orchestrations.PersonPets;
+
+namespace xChanger.Core.POC.Services.Coordinations
+{
+    public interface IExternalPersonWithPetsCoordinationService
+    {
+        ValueTask<List<PersonPet>> CoordinateExternalPersons();
+    }
+}
