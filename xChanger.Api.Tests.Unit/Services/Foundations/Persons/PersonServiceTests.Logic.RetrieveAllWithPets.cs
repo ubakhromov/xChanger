2d42ff0c468884133e95ec902a4696c24625ecc9
Ulugbek <ubakhromov22@gmail.com>
2022-12-22 16:53:38 +0500

    ShouldRetrievePersonsWithPets -> PASS

---
+++ xChanger.Api.Tests.Unit/Services/Foundations/Persons/PersonServiceTests.Logic.RetrieveAllWithPets.cs
@@ -0,0 +1,37 @@
+﻿using FluentAssertions;
+using Moq;
+using xChanger.Core.Models.Foundations.Persons;
+using Xunit;
+
+namespace xChanger.Core.Tests.Unit.Services.Foundations.Persons
+{
+    public partial class PersonServiceTests
+    {
+        [Fact]
+        public async Task ShouldRetrievePersonsWithPets()
+        {
+            //given
+            IQueryable<Person> randomPersonsWithPets = CreatedRandomPersons();
+            IQueryable<Person> storagePersonsWithPets = randomPersonsWithPets;
+            IQueryable<Person> expectedPersonsWithPets = storagePersonsWithPets;
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectAllPersonsWithPets())
+                    .Returns(storagePersonsWithPets);
+
+            //when
+            IQueryable<Person> actualPersons =
+                this.personService.RetrieveAllPersonsWithPets();
+
+            //then 
+            actualPersons.Should().BeEquivalentTo(expectedPersonsWithPets);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAllPersonsWithPets(),
+                    Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
